49f8e8c484a05f8d251d30e6e87f546aa4ff70d9
davidpazo <davidpazolopez@gmail.com>
2016-03-02 12:18:39 +0100

    more

---
+++ Login.py
@@ -0,0 +1,47 @@
+from gi.repository import Gtk
+from Taller import Taller
+class Login:
+    def __init__(self):
+
+            builder2 = Gtk.Builder()
+            builder2.add_from_file("inicio.glade")
+
+            self.nombre = builder2.get_object("nombre")
+            self.contraseña = builder2.get_object("contraseña")
+            self.ventanaEntrada = builder2.get_object("inicio")
+
+            sinais = {"on_Entrada_clicked": self.on_Entrada_clicked,
+                  "delete-event": self.cerrar}
+
+            builder2.connect_signals(sinais)
+            self.ventanaEntrada.set_title("Log in.")
+            self.ventanaEntrada.show_all()
+
+
+
+    # Método de entrada para poder acceder por usuario y contraseña a la segunda ventana
+    def on_Entrada_clicked(self, widget):
+        nombre = self.nombre.get_text();
+        contraseña = self.contraseña.get_text();
+        if nombre == "taller" and contraseña == "root":
+            nuevoObjeto = Taller()
+            self.ventanaEntrada.destroy()
+
+        else:
+            self.popup("Prueba otra vez")
+
+    def cerrar(self, widget):
+        widget.destroy()
+
+    def popup(self, texto):
+        window = Gtk.Window(title="Warning")
+        label = Gtk.Label(texto)
+        label.set_padding(15,15)
+        window.add(label)
+        window.connect("delete-event", self.cerrar)
+        window.set_position(Gtk.PositionType.RIGHT)
+        window.show_all()
+
+
+Login()
+Gtk.main()
--- Taller.py
+++ Taller.py
@@ -1,25 +1,8 @@
 import sqlite3 as dbapi
 from gi.repository import Gtk
 
-class taller:
+class Taller:
 
-    fichero2 = "inicio.glade"
-    builder2 = Gtk.Builder()
-    builder2.add_from_file(fichero2)
-    lnombre = builder2.get_object("lnombre")
-    lcontraseña = builder2.get_object("lcontraseña")
-    nombre = builder2.get_object("nombre")
-    contraseña = builder2.get_object("contraseña")
-    lprueba = builder2.get_object("lprueba")
-    ventanaEntrada = builder2.get_object("inicio")
-    ventanaEntrada.show_all()
-    # Conexión con la base de datos, y conectar a la interface
-    bd = dbapi.connect("basedatos.dat")
-    fichero = "Taller.glade"
-    print(bd)
-    cursor = bd.cursor()
-    builder = Gtk.Builder()
-    builder.add_from_file(fichero)
    # cursor.execute("CREATE TABLE taller (matricula VARCHAR(7) PRIMARY KEY NOT NULL,"
     #                        "vehiculo VARCHAR(20),"
      #                       "kilometros INT,"
@@ -30,18 +13,25 @@ class taller:
           #                  "direccion VARCHAR(10))")
 
     def __init__(self):
-        self.ventanaEntrada.show_all();
+        #Conexión con la base de datos, y conectar a la interface
+        self.bd = dbapi.connect("basedatos.dat")
+        self.cursor = self.bd.cursor()
+
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("Taller.glade")
+        self.inicializar()
+        self.ventana = self.builder.get_object("Taller")
+
         # Señales, aquí se declaran los métodos anteriormente comentados, para que al pulsar accedan
         sinais = {"on_insertar_clicked": self.on_insertar_clicked,
-                  "on_consultar_clicked": self.on_consultar_clicked,
-                  "on_borrar_clicked": self.on_borrar_clicked,
-                  "on_Modificar_clicked": self.on_Modificar_clicked,
-                  "on_Entrada_clicked": self.on_Entrada_clicked,
-                  "delete-event": Gtk.main_quit}
-        self.builder2.connect_signals(sinais)
+                      "on_consultar_clicked": self.on_consultar_clicked,
+                      "on_borrar_clicked": self.on_borrar_clicked,
+                      "on_Modificar_clicked": self.on_Modificar_clicked,
+                      "delete-event": Gtk.main_quit}
+
         self.builder.connect_signals(sinais)
-
-
+        self.ventana.set_title("Taller.")
+        self.ventana.show_all()
 
 
     def inicializar(self):
@@ -71,23 +61,13 @@ class taller:
             columna = Gtk.TreeViewColumn(title, render, text=i)
             self.vista.append_column(columna)
 
-     # Método de entrada para poder acceder por usuario y contraseña a la segunda ventana
-    def on_Entrada_clicked(self, entrada):
-        nombre = self.nombre.get_text();
-        contraseña = self.contraseña.get_text();
-        if nombre == "taller" and contraseña == "merda":
-            self.lprueba.set_text(" %s" % "Peritas estas dentro")
-            self.ventana = self.builder.get_object("Taller")
-            self.ventana.set_visible(True)
 
-            self.ventanaEntrada.hide();
-            self.inicializar()
-            self.ventana.show_all()
-        else:
-            self.lprueba.set_text("Prueba otra vez")
 
     # Método Consultar, imprime los datos de la base por consola
     def on_consultar_clicked(self, control):
+        self.actualizar()
+
+    def actualizar(self):
         self.lista.clear()
         self.cursor.execute("select * from taller")
         #print(self.cursor.fetchall())
@@ -96,13 +76,16 @@ class taller:
 
         self.vista.set_model(self.lista)
 
+
+
     # Método Borrar
     def on_borrar_clicked(self, borrar):
         matricula = self.builder.get_object("matricula").get_text()
-        print("El evento está siendo borrado por código")
+        #print("El evento está siendo borrado por código")
         self.cursor.execute("delete from taller where matricula ='" + matricula + "'")
-        print("Borrado")
+        self.popup("Borrado")
         self.bd.commit()
+        self.actualizar()
 
     # Método Modificar. Modifica a través de la primary Key
     def on_Modificar_clicked(self, modificar):
@@ -123,7 +106,7 @@ class taller:
                                              ",cif='" + cifnif +"'"
                                              ",telefono='" + telefono +"'"
                                              ",direccion='" + direccion +"' where matricula='" + matricula + "'")
-        print("Modificado")
+        self.popup("Modificado")
         self.bd.commit()
 
     # Método insertar
@@ -137,6 +120,7 @@ class taller:
         telefono = self.builder.get_object("telefono").get_text()
         direccion = self.builder.get_object("direccion").get_text()
 
+
         print("inserte")
         self.cursor.execute(
             "insert into taller values('" + matricula + "'"
@@ -147,9 +131,19 @@ class taller:
                                      ",'" + cifnif +"'"
                                      ",'" + telefono +"'"
                                      ",'" + direccion +"')")
-        print("Insertado")
+        self.popup("Insertado")
         # Siempre se debe hacer un commit al final de cada evento
         self.bd.commit()
 
-taller()
-Gtk.main()
+    def cerrar(self, widget):
+        widget.destroy()
+
+    def popup(self, texto):
+        window = Gtk.Window(title="Aviso")
+        label = Gtk.Label(texto)
+        label.set_padding(15, 15)
+        window.add(label)
+        window.connect("delete-event", self.cerrar)
+        window.set_position(Gtk.PositionType.RIGHT)
+        window.show_all()
+
